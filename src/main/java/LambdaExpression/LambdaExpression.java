1fa77c27a32914217abf1d03371515f8a26e638b
Tejeswar0475 <tejeswar096@gmail.com>
2021-01-10 14:13:40 +0530

    Added code for validation of user entered data using lambdaexpression with exception

--- src/main/java/LambdaExpression/LambdaExpression.java
+++ src/main/java/LambdaExpression/LambdaExpression.java
@@ -1,7 +1,6 @@
 package LambdaExpression;
 
 import com.google.common.base.Predicate;
-
 import LambdaExpression.InvalidInputException;
 
 public class LambdaExpression {
@@ -10,4 +9,44 @@ public class LambdaExpression {
 	{
 		return welcomeMessage;
 	}
+	
+	boolean nameTest(String name) throws InvalidInputException
+    {
+        Predicate<String> matcher = n ->(n.matches("^[A-Z]{1}[A-Za-z]{2,12}$"));
+        if (matcher.test(name))
+            return true;
+        else
+            throw new InvalidInputException("plz enter name with First Letter Capital ");
+
+    }
+	
+    boolean mobileNumberTest(String number) throws InvalidInputException
+    {
+        Predicate<String> matcher = n ->(n.matches("^[0-9]{2}\s[1-9]{1}[0-9]{9}$"));
+        boolean result=matcher.test(number);
+        if (result)
+            return true;
+        else
+            throw new InvalidInputException("plz enter phone number n ths :91 1234567890: format");
+    }
+    
+    boolean emailTest(String email)	throws InvalidInputException
+    {
+       Predicate<String> matcher = n ->(n.matches("^([a-zA-Z0-9]+)([_+.-]{1}[a-z0-9]+)*@([A-Za-z0-9]+).([a-z]{2,4})((.[a-z]{2,3})?)$"));
+        boolean result=matcher.test(email);
+        if (result)
+            return true;
+        else
+            throw new InvalidInputException("Email format ex: abc.xyz@gmail.com.on with xyz and .in optional ");
+    }
+  
+    boolean passwordTest(String password) throws InvalidInputException
+    {
+        Predicate<String> matcher = n ->(n.matches("^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!_%*#?&]{1})[a-zA-Z0-9@$!_%*#?&]{8,}$"));
+        boolean result=matcher.test(password);
+        if (result)
+            return true;
+        else
+            throw new InvalidInputException("Password should have at minimum one uppercase and one numeric and exctly one special charecters");
+    }
 }
--- src/test/java/LambdaExpression/InvalidaInputExceptionTest.java
+++ src/test/java/LambdaExpression/InvalidaInputExceptionTest.java
@@ -3,6 +3,8 @@ package LambdaExpression;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import LambdaExpression.LambdaExpression;
+
 public class InvalidaInputExceptionTest {
 	
 	@Test
@@ -13,4 +15,81 @@ public class InvalidaInputExceptionTest {
 		assertSame("Welcome to user registration",welcomeMessageResult);
 	}
 
+	@Test
+	public void NameTest()
+	{
+		try {
+			try {
+				LambdaExpression name = new LambdaExpression();            	
+                boolean output = name.nameTest("ilta");
+                assertTrue(output);
+			}
+            catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+        } catch (InvalidInputException e) {
+        	e.printStackTrace();
+        }
+	}
+
+	@Test
+	public void EmailIdTest()
+	{
+		try
+		{	try {
+				LambdaExpression newEmail = new LambdaExpression();
+				boolean output = newEmail.emailTest("abc1234@gmail.com");
+				assertTrue(output);
+            }
+            catch (NullPointerException e)
+             {
+            	e.printStackTrace();
+             }
+       }
+       catch(InvalidInputException e)
+       {
+           e.printStackTrace();
+       }
+    }
+
+	@Test
+	public void MobileTest()
+	{
+		try
+		{	try	{
+    	   		LambdaExpression phoneNumber = new LambdaExpression();
+                boolean output = phoneNumber.mobileNumberTest("91 1204356789");
+                assertTrue(output);
+            }
+       		catch (NullPointerException e)
+       		{
+       			e.printStackTrace();
+       		}
+		}
+       catch(InvalidInputException e)
+       {
+           e.printStackTrace();
+       }
+	}
+
+	@Test
+    public void PasswordTest()
+    {
+       try
+       {    try	{
+    	   		LambdaExpression passWord = new LambdaExpression();
+    	   		boolean output = passWord.passwordTest("Thomas#2@87B");
+    	   		assertTrue(output);
+            }
+            catch (NullPointerException e)
+            {
+                e.printStackTrace();
+            }
+       }
+       catch(InvalidInputException e)
+       {
+           e.printStackTrace();
+       }
+    }
+	
 }
